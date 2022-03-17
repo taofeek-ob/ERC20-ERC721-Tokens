dea5980397aa6702156be9b9e85828ea638c65fb
taofeek-ob <taofeek01@yahoo.com>
2022-03-18 00:02:18 +0100

    third

---
+++ ERC721 Token/contracts/TaoNFT.sol
@@ -0,0 +1,254 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+
+library Base64 {
+    string internal constant TABLE_ENCODE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    bytes  internal constant TABLE_DECODE = hex"0000000000000000000000000000000000000000000000000000000000000000"
+                                            hex"00000000000000000000003e0000003f3435363738393a3b3c3d000000000000"
+                                            hex"00000102030405060708090a0b0c0d0e0f101112131415161718190000000000"
+                                            hex"001a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132330000000000";
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return '';
+
+        // load the table into memory
+        string memory table = TABLE_ENCODE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+
+            // run over the input, 3 bytes at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+                // read 3 bytes
+                dataPtr := add(dataPtr, 3)
+                let input := mload(dataPtr)
+
+                // write 4 characters
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(18, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(12, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(shr( 6, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(        input,  0x3F))))
+                resultPtr := add(resultPtr, 1)
+            }
+
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+        }
+
+        return result;
+    }
+
+    function decode(string memory _data) internal pure returns (bytes memory) {
+        bytes memory data = bytes(_data);
+
+        if (data.length == 0) return new bytes(0);
+        require(data.length % 4 == 0, "invalid base64 decoder input");
+
+        // load the table into memory
+        bytes memory table = TABLE_DECODE;
+
+        // every 4 characters represent 3 bytes
+        uint256 decodedLen = (data.length / 4) * 3;
+
+        // add some extra buffer at the end required for the writing
+        bytes memory result = new bytes(decodedLen + 32);
+
+        assembly {
+            // padding with '='
+            let lastBytes := mload(add(data, mload(data)))
+            if eq(and(lastBytes, 0xFF), 0x3d) {
+                decodedLen := sub(decodedLen, 1)
+                if eq(and(lastBytes, 0xFFFF), 0x3d3d) {
+                    decodedLen := sub(decodedLen, 1)
+                }
+            }
+
+            // set the actual output length
+            mstore(result, decodedLen)
+
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+
+            // run over the input, 4 characters at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+               // read 4 characters
+               dataPtr := add(dataPtr, 4)
+               let input := mload(dataPtr)
+
+               // write 3 bytes
+               let output := add(
+                   add(
+                       shl(18, and(mload(add(tablePtr, and(shr(24, input), 0xFF))), 0xFF)),
+                       shl(12, and(mload(add(tablePtr, and(shr(16, input), 0xFF))), 0xFF))),
+                   add(
+                       shl( 6, and(mload(add(tablePtr, and(shr( 8, input), 0xFF))), 0xFF)),
+                               and(mload(add(tablePtr, and(        input , 0xFF))), 0xFF)
+                    )
+                )
+                mstore(resultPtr, shl(232, output))
+                resultPtr := add(resultPtr, 3)
+            }
+        }
+
+        return result;
+    }
+}
+
+contract TaoNFT is ERC721Enumerable, Ownable {
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
+    // string tokenName;
+    // string tokenImg;
+    // string tokenNo;
+    Counters.Counter private _tokenIds;
+    
+    uint public constant MAX_SUPPLY = 9;
+    
+    string public baseTokenURI;
+
+     
+    constructor() ERC721("TaoNFT-V1", "TNT") {
+       
+        
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+    
+    // constructor(string memory baseURI) ERC721("TaoNFT", "TNT") {
+    //     setBaseURI(baseURI);
+        
+    // }
+    
+//     {
+//   "description": "Friendly OpenSea Creature that enjoys long swims in the ocean.", 
+//   "external_url": "https://openseacreatures.io/3", 
+//   "image": "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png", 
+//   "name": "Dave Starbelly",
+//   "attributes": [ ... ], 
+// }
+    
+    // function _baseURI() internal view virtual override returns (string memory) {
+    //     return baseTokenURI;
+    // }
+    
+    // function setBaseURI(string memory _baseTokenURI) public onlyOwner {
+    //     baseTokenURI = _baseTokenURI;
+    // }
+    
+    function mintNFTs(uint _count) public {
+        uint totalMinted = _tokenIds.current();
+
+        require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs left!");
+        require(_count ==1, "Cannot mint specified number of NFTs.");
+
+        for (uint i = 0; i < _count; i++) {
+            _mintSingleNFT();
+        }
+    }
+    
+    function _mintSingleNFT() private {
+        uint newTokenID = _tokenIds.current();
+        _safeMint(msg.sender, newTokenID);
+        _tokenIds.increment();
+    }
+    
+    function tokensOfOwner(address _owner) external view returns (uint[] memory) {
+
+        uint tokenCount = balanceOf(_owner);
+        uint[] memory tokensId = new uint256[](tokenCount);
+
+        for (uint i = 0; i < tokenCount; i++) {
+            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokensId;
+    }
+      function concatenate(uint256 tokenId) private pure returns (string memory){
+     string memory img= "https://ipfs.io/ipfs/QmPa5q8VnemW1yLUkQvKvmXGQXxLt5bb8Gji3RLGh4DXwW/";
+     string memory png= ".png";
+      string memory tokenImg= uint2str(tokenId);
+     
+      
+       return string(abi.encodePacked(img,tokenImg,png));
+    } 
+
+    function concatenateName(uint256 tokenId) private pure  returns (string memory){
+     string memory tokenNo=uint2str(tokenId);
+      string memory tokenName ="TaoNFT-V1 ";
+      
+       return string(abi.encodePacked(tokenName,'#',tokenNo));
+    } 
+
+    
+
+    function tokenURI(uint256 tokenId) override(ERC721) public pure returns (string memory) {
+       
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"Name": "', concatenateName(tokenId), '", "description": "A Collection of 9 TestNet NFTS for Blockgames", "image": "', concatenate(tokenId), '"}'))));
+
+        return string(abi.encodePacked('data:application/json;base64,', json));
+    }   
+    
+}
+
+
+
+ 
+
+
+ 
